f872b3b8b5affea3335de3f5090afda5dc3e8381
Simon LANOS <simon.lanos99@gmail.com>
2018-10-17 17:19:12 +0200

    feat:(helper): ajoute un helper de log

---
+++ Helper/Log.php
@@ -0,0 +1,20 @@
+<?php
+
+
+class Log
+{
+  public static function logWrite($data)
+  {
+      $directory = "/Log";
+      $file = date('Y-m-d'). ".log";
+      $path = dirname(__DIR__) . $directory . $file;
+      $data = date('H:i:s') . " " . $data;
+      $handle = fopen($path, "a");
+
+      if(flock($handle, LOCK_EX)){
+          fwrite($handle, $data . PHP_EOL);
+          flock($handle, LOCK_UN);
+          fclose($handle);
+      }
+  }
+}
