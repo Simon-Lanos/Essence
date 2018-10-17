cce153e4d96d4fe0756d453852472cc7e1d8e68f
Simon LANOS <simon.lanos99@gmail.com>
2018-10-17 17:20:13 +0200

    feat(response): ajoute un type de reponce Data pour les retours en JSON

---
+++ Response/Data.php
@@ -0,0 +1,36 @@
+<?php
+
+class Data
+{
+    protected $json;
+    protected $formated;
+
+    public function __construct(array $data)
+    {
+        $this->json = $data;
+        $this->formated = false;
+        return $this->getJson();
+    }
+
+    private function formatJson()
+    {
+        $this->json = json_encode($this->json);
+        $this->formated = true;
+        return $this;
+    }
+
+    private function isFomated()
+    {
+        return $this->formated;
+    }
+
+    public function getJson()
+    {
+        if ($this->isFomated()) {
+            return $this->json;
+        }
+        else {
+            return $this->formatJson()->json;
+        }
+    }
+}
