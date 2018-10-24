c26b3fa9657f01a3a1ec117722421da9520ada92
Simon LANOS <simon.lanos99@gmail.com>
2018-10-24 15:04:40 +0200

    fix(log helper): change le chemin utilisé dans le Log helper qui ne pointait pas dans le bon fichier

--- Helper/Log.php
+++ Helper/Log.php
@@ -6,9 +6,9 @@ class Log
 {
   public static function logWrite($data)
   {
-      $directory = "/Log";
+      $directory = "Log";
       $file = date('Y-m-d'). ".log";
-      $path = dirname(__DIR__) . $directory . $file;
+      $path = dirname(__DIR__) . DIRECTORY_SEPARATOR . $directory . DIRECTORY_SEPARATOR . $file;
       $data = date('H:i:s') . " " . $data;
       $handle = fopen($path, "a");
 
